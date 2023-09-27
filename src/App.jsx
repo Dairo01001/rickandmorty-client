be8d8211a24488962d2811948b9dd900ecdc5a91
Dairo García Naranjo <50602257+Dairo01001@users.noreply.github.com>
2023-09-27 12:25:41 -0500

    config router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-function App () {
-  return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello world!
-    </h1>
-  )
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Hello World!</h1>
+  }
+])
+
+const App = () => {
+  return <RouterProvider router={router} />
 }
 
 export default App
